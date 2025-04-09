932b902e79545a35dce01768742b8a0792877937
Ahmed Solanki <ahmedsolanki.dev@gmail.com>
2025-04-09 16:09:33 +0530

    changes

--- src/app/raffle-draw/attendance-list/page.jsx
+++ src/app/raffle-draw/attendance-list/page.jsx
@@ -1,12 +1,15 @@
 import AttendanceList from "@/components/raffleDraw/attendance/AttendanceList";
 import Copyright from "@/components/layouts/copyright/copyright";
 import Navbar from "@/components/layouts/navbar/navbar";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div>
       <Navbar />
-      <AttendanceList />
+      <Suspense fallback={<div>Loading attendance list...</div>}>
+        <AttendanceList />
+      </Suspense>
       <Copyright />
     </div>
   );
